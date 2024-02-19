07033ecfaadf0d3da99ca03dd26a6f8710a12d61
mirzafar <m_mirzafar@mail.ru>
2024-02-19 11:50:16 +0600

    f

--- server.py
+++ server.py
@@ -2,52 +2,62 @@ import asyncio
 import os
 import uuid
 
-import scipy
-from transformers import AutoProcessor, MusicgenForConditionalGeneration
+# import scipy
+# from transformers import AutoProcessor, MusicgenForConditionalGeneration
 
 from httpclient import request
 from settings import settings
 from tgclient import tgclient
 
-processor = AutoProcessor.from_pretrained('facebook/musicgen-small')
-model = MusicgenForConditionalGeneration.from_pretrained('facebook/musicgen-small')
+
+# processor = AutoProcessor.from_pretrained('facebook/musicgen-small')
+# model = MusicgenForConditionalGeneration.from_pretrained('facebook/musicgen-small')
 
 
 async def generate(order):
-    inputs = processor(
-        text=[' '.join(order['words'])],
-        padding=True,
-        return_tensors='pt',
-    )
-
-    uid = str(uuid.uuid4())
-    root_dir = settings['root_dir'] + '/static/uploads/' + f'{uid[:2]}/{uid[2:4]}'
-    if not os.path.isdir(root_dir):
-        os.makedirs(root_dir)
-
-    audio_values = model.generate(**inputs, max_new_tokens=1024)
-    sampling_rate = model.config.audio_encoder.sampling_rate
-    scipy.io.wavfile.write(f'{root_dir}/{uid}.mp3', rate=sampling_rate, data=audio_values[0, 0].numpy())
-
-    await tgclient.api_call(
-        method_name='sendPhoto',
-        payload={
-            'chat_id': order['chat_id'],
-            'photo': f'{settings["base_url"]}/static/uploads/{uid[:2]}/{uid[2:4]}/{uid}.mp3'
-        }
-    )
-
-    await request(
-        method='post',
-        url='https://art.ttshop.kz/orders/',
-        json={
-            'action': 'done',
-            'order': {
-                'id': order['id'],
-                'path': f'{uid[:2]}/{uid[2:4]}/{uid}.mp3'
+    # inputs = processor(
+    #     text=[' '.join(order['words'])],
+    #     padding=True,
+    #     return_tensors='pt',
+    # )
+    #
+    # uid = str(uuid.uuid4())
+    # root_dir = settings['root_dir'] + '/static/uploads/' + f'{uid[:2]}/{uid[2:4]}'
+    # if not os.path.isdir(root_dir):
+    #     os.makedirs(root_dir)
+    #
+    # audio_values = model.generate(**inputs, max_new_tokens=1024)
+    # sampling_rate = model.config.audio_encoder.sampling_rate
+    # scipy.io.wavfile.write(f'{root_dir}/{uid}.mp3', rate=sampling_rate, data=audio_values[0, 0].numpy())
+    #
+    # await tgclient.api_call(
+    #     method_name='sendPhoto',
+    #     payload={
+    #         'chat_id': order['chat_id'],
+    #         'photo': f'{settings["base_url"]}/static/uploads/{uid[:2]}/{uid[2:4]}/{uid}.mp3'
+    #     }
+    # )
+    print(
+        await tgclient.api_call(
+            method_name='sendMessage',
+            payload={
+                'chat_id': order['chat_id'],
+                'text': str(order['id'])
             }
-        }
+        )
     )
+
+    # await request(
+    #     method='post',
+    #     url='https://art.ttshop.kz/orders/',
+    #     json={
+    #         'action': 'done',
+    #         'order': {
+    #             'id': order['id'],
+    #             'path': f'{uid[:2]}/{uid[2:4]}/{uid}.mp3'
+    #         }
+    #     }
+    # )
 
 
 async def main():
