274027f9c45e7461d3c1cb3cd5f9d3322b2e83e5
mirzafar <m_mirzafar@mail.ru>
2024-02-19 15:36:28 +0600

    f

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from cache import cache
 from db import db
 from replicate import Client
 
@@ -25,65 +26,127 @@ class App:
 async def main():
     client = Client(api_token='r8_agYdIyP42udjzCiMulRpAEdB4jLPrvw2DbB8v')
     await db.initialize(App(), loop)
+    await cache.initialize(loop)
 
     while True:
-        order = await db.fetchrow(
+        playlist = await db.fetchrow(
             '''
-            SELECT *
-            FROM public.orders
-            WHERE status = 0
+            SELECT p.*, c.uid AS chat_id
+            FROM public.playlist p
+            LEFT JOIN public.customers c ON c.id = p.customer_id
+            WHERE p.status = 0 AND p.words IS NOT NULL
             '''
         )
 
-        if order:
+        if playlist:
             await db.fetchrow(
                 '''
-                UPDATE public.orders
+                UPDATE public.playlist
                 SET status = 1
                 WHERE id = $1
                 ''',
-                order['id'],
+                playlist['id'],
             )
-            loop.create_task(generate(order, client))
+            loop.create_task(generate(playlist, client))
 
         await asyncio.sleep(5)
 
 
-async def generate(order, client):
-    output = await client.async_run(
-        'meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38',
-        input={
-            'top_k': 250,
-            'top_p': 0,
-            'prompt': ', '.join(order['words']),
-            'duration': 10,
-            'temperature': 1,
-            'continuation': False,
-            'model_version': 'stereo-large',
-            'output_format': 'mp3',
-            'continuation_start': 0,
-            'multi_band_diffusion': False,
-            'normalization_strategy': 'peak',
-            'classifier_free_guidance': 3
+async def generate(playlist, client):
+    try:
+        output = await client.async_run(
+            'meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38',
+            input={
+                'top_k': 250,
+                'top_p': 0,
+                'prompt': ', '.join(playlist['words']),
+                'duration': 10,
+                'temperature': 1,
+                'continuation': False,
+                'model_version': 'stereo-large',
+                'output_format': 'mp3',
+                'continuation_start': 0,
+                'multi_band_diffusion': False,
+                'normalization_strategy': 'peak',
+                'classifier_free_guidance': 3
+            }
+        )
+        await tgclient.api_call(
+            method_name='sendAudio',
+            payload={
+                'chat_id': playlist['chat_id'],
+                'audio': str(output)
+            }
+        )
+    except (Exception,):
+        tune = await db.fetchrow(
+            '''
+            SELECT *
+            FROM public.tunes
+            ORDER BY (
+                SELECT COUNT(*)
+                FROM unnest(words) AS element1
+                INNER JOIN unnest($1::text[]) AS element2 ON element1 = element2
+            ) DESC
+            LIMIT 1;
+            ''',
+            list(set(playlist['words']))
+        )
+
+        output = settings['base_url'] + '/static/uploads/' + tune['path'] if tune else None
+
+    if not output:
+        await db.fetchrow(
+            '''
+            UPDATE public.playlist
+            SET status = 2
+            WHERE id = $1
+            ''',
+            playlist['id']
+        )
+
+        return await tgclient.api_call(
+            method_name='sendMessage',
+            payload={
+                'chat_id': playlist['chat_id'],
+                'text': 'Ничего не найдено',
+                'reply_markup': {
+                    'keyboard': [[{
+                        'text': '🏠 Вернуться в меню',
+                    }]],
+                    'one_time_keyboard': True,
+                    'resize_keyboard': True
+                }
+            }
+        )
+
+    await cache.setex(f'art:telegram:audio:{playlist["customer_id"]}', 600, playlist['id'])
+    await tgclient.api_call(
+        method_name='sendAudio',
+        payload={
+            'chat_id': playlist['chat_id'],
+            'audio': output,
+            'reply_markup': {
+                'keyboard': [
+                    [{'text': '\u2069Сохранить трек'}],
+                    [{
+                        'text': '🏠 Вернуться в меню',
+                    }]
+                ],
+                'one_time_keyboard': True,
+                'resize_keyboard': True
+            }
         }
     )
 
     await db.fetchrow(
         '''
-        UPDATE public.orders
+        UPDATE public.playlist
         SET status = 2, url = $2
         WHERE id = $1
         ''',
-        order['id'],
+        playlist['id'],
         str(output)
-    )
-
-    await tgclient.api_call(
-        method_name='sendAudio',
-        payload={
-            'chat_id': order['chat_id'],
-            'audio': str(output)
-        }
     )
 
 
